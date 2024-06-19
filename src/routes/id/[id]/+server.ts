c63a57dea3b849ec3639c99d4a74b6ba6436878f
ByteAtATime <ByteAtATime@proton.me>
2024-06-19 14:01:35 -0700

    feat: get icon by id

---
+++ src/routes/id/[id]/+server.ts
@@ -0,0 +1,44 @@
+import type { RequestHandler } from "./$types";
+import { error } from '@sveltejs/kit';
+import { read } from '$app/server';
+
+const getIconById = async (id: number): Promise<Response|null> => {
+	try {
+		const icon = await import(`$lib/icons/${id}.svg`);
+
+		const response = read(icon.default);
+
+		response.headers.set('Content-Type', 'image/svg+xml');
+
+		return response;
+	} catch (e) {
+		console.error(e);
+		return null;
+	}
+}
+
+export const GET: RequestHandler = async ({params, url}) => {
+	const {id: rawId} = params;
+
+	const id = parseInt(rawId);
+
+	if (!id) error(400, `Invalid chain id ${id}`);
+
+	const icon= await getIconById(id);
+
+	if (icon) return icon;
+
+	const rawFallback = url.searchParams.get("fallback");
+
+	if (!rawFallback) return error(404, `Icon not found for chain id ${id}`);
+
+	const fallback = parseInt(rawFallback);
+
+	if (fallback) {
+		const fallbackIcon = await getIconById(fallback);
+
+		if (fallbackIcon) return fallbackIcon;
+	}
+
+	return error(404, `Icon not found for chain id ${id} and fallback ${fallback}`);
+}
