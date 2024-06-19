05d8e0f0908f852459a45af64cd14b9106745708
ByteAtATime <ByteAtATime@proton.me>
2024-06-19 14:16:29 -0700

    feat: get icon by slug

---
+++ src/routes/slug/[slug]/+server.ts
@@ -0,0 +1,32 @@
+import type { RequestHandler } from './$types';
+import { error } from '@sveltejs/kit';
+import { getIconById } from '$lib/icon';
+
+const SLUG_TO_CHAIN_ID: Record<string, number> = {
+	ethereum: 1,
+	mainnet: 1,
+	optimism: 10,
+	optimistic: 10,
+};
+
+export const GET: RequestHandler = async ({ params, url }) => {
+	const { slug } = params;
+	const id = SLUG_TO_CHAIN_ID[slug];
+
+	const icon = await getIconById(id);
+
+	if (icon) return icon;
+
+	const fallback = url.searchParams.get('fallback');
+	if (!fallback) return error(404, `Icon not found for chain id ${id} (retrieved with slug "${slug}")`);
+
+	const fallbackId = SLUG_TO_CHAIN_ID[fallback];
+
+	if (fallback) {
+		const fallbackIcon = await getIconById(fallbackId);
+
+		if (fallbackIcon) return fallbackIcon;
+	}
+
+	return error(404, `Icon not found for slug "${slug}" and fallback slug "${fallback}"`);
+};
