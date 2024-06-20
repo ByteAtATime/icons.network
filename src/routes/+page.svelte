64a2ee73dbcd200cbfc6934256f95f6425580d3b
ByteAtATime <ByteAtATime@proton.me>
2024-06-19 19:00:59 -0700

    feat: home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,48 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main>
+	<h1>Network icons shouldn't be this hard to get</h1>
+	<p>So I made this simple tool, to allow you to get them as an SVG.</p>
+
+	<p>You can get them by chain ID...</p>
+
+	<div>
+		<div><img src="/id/1" alt="Ethereum" /> <code>&lt;img src="https://icons.network/id/1" /&gt;</code></div>
+		<div><img src="/id/10" alt="Optimism" /> <code>&lt;img src="https://icons.network/id/10" /&gt;</code></div>
+	</div>
+
+	<p>Or by network slug (more readable, as long as it's not user input)...</p>
+
+	<div>
+		<div><img src="/slug/mainnet" alt="Ethereum" /> <code>&lt;img src="https://icons.network/slug/mainnet" /&gt;</code></div>
+		<div><img src="/slug/optimism" alt="Optimism" /> <code>&lt;img src="https://icons.network/slug/optimism" /&gt;</code></div>
+	</div>
+</main>
+
+<style>
+	main {
+      font-family: system-ui, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+			font-size: 1.5rem;
+
+			display: flex;
+			flex-direction: column;
+			gap: 12px 0;
+			align-items: center;
+			text-align: center;
+  }
+
+	main > div {
+		display: flex;
+		flex-direction: column;
+			gap: 24px 0;
+	}
+
+	main > div > div {
+		display: flex;
+		align-items: center;
+			gap: 0 8px;
+	}
+
+	main > div > div > img {
+		width: 48px;
+		height: 48px;
+	}
+</style>
